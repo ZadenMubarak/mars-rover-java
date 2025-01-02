a76a6f9751cd050ff17df5d175a0b48ec7016d45
Zaden Ngobeni <zangobejhb024@student.wethinkcode.co.za>
2025-01-02 13:34:15 +0200

    first commit

---
+++ SimulateRover.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class SimulateRover{
+
+    public static void main (String[] args){
+
+        //mars rover project
+
+        if (args.length != 1){
+            System.out.println("Error not enough arguments");
+            System.exit(1);
+
+        }
+    }
+
+    //outside main scope
+
+    public static void simulate(String file){
+
+        //read file first
+
+        //see you l8er
+    }
+}
